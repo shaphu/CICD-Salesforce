6dce8e2a78f0cbcf9d8fed9f5d07c20b25f3008d
niristotle okram <nirish.okram@gmail.com>
2018-03-27 13:13:42 -0500

    Update deleteGitTags.sh

--- deleteGitTags.sh
+++ deleteGitTags.sh
@@ -1,6 +1,9 @@
-echo "regex to search for the git tags: $1"
-git tag --list | grep $1 > tag_list_to_delete
-echo "starting the loop"
+#!/bin/bash
+echo "regex to search for the git tags: $tagRegex"
+git tag --list | grep $tagRegex > tag_list_to_delete
+echo "list of git tags to be deleted: "
+cat tag_list_to_delete
+echo "starting the loop to delete the above list......................"
 for i in `cat tag_list_to_delete`
 do
 	echo "Deleting the git tag locally: $i"
